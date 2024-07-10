bf8ed644b02493508dc179d3608d2c4ec4d55028
rrivera <rrivera@redbannermedia.com>
2024-07-10 13:43:26 -0400

    setting up Forgot Password UI

--- src/views/Forgot.vue
+++ src/views/Forgot.vue
@@ -9,7 +9,7 @@ import LockIcon from '../components/icons/LockIcon.vue';
 import MailIcon from '../components/icons/MailIcon.vue';
 
 import { reactive, computed } from 'vue';
-import { required, email, minLength, sameAs, alpha, alphaNum } from '@vuelidate/validators';
+import { required, email } from '@vuelidate/validators';
 import { useVuelidate } from '@vuelidate/core';
 
 import { postApi } from '../axios.ts';
@@ -19,14 +19,12 @@ const router = useRouter();
 // Form State
 const form = reactive({
 	email: '',
-	password: '',
 });
 
 // Rules for vuelidate validation
 const rules = computed(() => {
 	return {
 		email: { required, email },
-		password: { required, minLength: minLength(8) },
 	}
 });
 
