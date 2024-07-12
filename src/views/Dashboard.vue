864258aa66f5f9584acfe897adb96e57fb4ffb03
Roberto Rivera <rrivera@redbannermedia.com>
2024-07-12 15:25:41 -0400

    switching devices

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import {getApi} from '../axios.ts';
 
+const getDashboard = async () => {
+	try {
+		console.log(localStorage.getItem('token'));
+		const response = await getApi('/notebooks');
+		console.log(response);
+	} catch (error) {
+		console.error(error);
+	}
+	console.log('Dashboard');
+}
+
+getDashboard();
 </script>
 <template>
 	<div>
