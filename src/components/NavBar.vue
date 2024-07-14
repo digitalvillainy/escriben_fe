3418696527ac5a4b83a1fcb5ed53228fa8921020
rrivera <rrivera@redbannermedia.com>
2024-07-14 15:21:05 -0400

    Fixed issues with Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -15,7 +15,7 @@
 	<div class="flex flex-row justify-between place-items-center text-white font-antonio bg-black px-3 text-xl">
 		<RouterLink to="/" class="m-2 text-shadow hover:text-cyan-400">Home</RouterLink>
 		<span class="text-2xl">-Escriben-</span>
-		<div class="flex flex-row place-items-center" v-if="authenticated">
+		<div class="flex flex-row place-items-center" v-if="!authenticated">
 			<RouterLink to="/register" class="m-2 text-shadow hover:text-cyan-400">Register</RouterLink>
 			<RouterLink to="/login" class="m-2 text-shadow hover:text-cyan-400">Login</RouterLink>
 		</div>
