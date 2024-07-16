0a3d4ffa7bab6207b2a75130ae7923f6e1b014ca
rrivera <rrivera@redbannermedia.com>
2024-07-16 16:54:44 -0400

    fixed styling and ref

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,12 +6,13 @@ import PlusIcon from '../components/icons/PlusIcon.vue';
 
 import {getApi, postApi} from '../axios.ts';
 import {useUserStore} from '../stores/user';
-import {onMounted} from 'vue';
+import {onMounted, ref} from 'vue';
 
 
 const userStore = useUserStore();
 
-let notebooks: any;
+let notebooks: array<object> = ref([{}]);
+
 const getDashboard = async () => {
 	try {
 		notebooks = await getApi(`/notebooks?user_id=${userStore.id}`);
@@ -26,7 +27,7 @@ const createNotebook = async () => {
 
 	} catch (error) {
 		console.error(error);
-	}
+}
 
 	await getDashboard();
 };
@@ -36,10 +37,9 @@ onMounted(() => {
 	getDashboard();
 });
 
-
 </script>
 <template>
-
+	<!-- TODO: Add current notebooks and if not a prompt to create -->
 	<section class="flex flex-col h-screen">
 		<NavBar />
 		<main class="flex flex-col place-items-center flex-grow">
@@ -47,19 +47,19 @@ onMounted(() => {
 			<div class="w-full flex flex-row justify-start px-12">
 				<StepCard 
 					@click="createNotebook" 
-					class="w-44 h-64 drop-shadow-2xl border-custom-cyan border-dashed border-8 flex flex-col cursor-pointer hover:bg-gray-600 hover:text-white">
+					class="w-44 h-64 drop-shadow-2xl border-custom-cyan border-dashed border-4 flex flex-col cursor-pointer hover:bg-gray-600 hover:text-white">
 					<PlusIcon class="mx-auto"/>
 					<span class="font-antonio text-lg text-center">Create Notebook</span>
 				</StepCard>
 			</div>
-			<div class="w-full flex flex-row justify-start bg-zinc-900 h-full space-x-4 mt-8 pt-8 px-10 overflow-y-scroll overflow-x-hidden">
+			<div class="w-full flex flex-row flex-wrap justify-start bg-zinc-900 h-full mt-8 pt-8 px-7 overflow-y-scroll overflow-x-hidden">
 
 				<StepCard 
 					v-for="( notebook, index ) in notebooks"
 					v-bind:key="index"
-					class="w-44 h-64 drop-shadow-2xl border-custom-cyan border-doshed border-4 flex flex-col cursor-pointer hover:bg-gray-600 hover:text-white">
-					<PlusIcon class="mx-auto"/>
-					<span class="font-antonio text-md text-center">{{notebook.title}}</span>
+					class="w-44 h-64 drop-shadow-2xl border-custom-cyan border-doshed border-4 
+					flex flex-col cursor-pointer hover:bg-gray-600 hover:text-white m-4 hover:border-cyan-400">
+					<span class="text-white font-antonio text-md text-center">{{notebook.title}}</span>
 				</StepCard>
 			</div>
 		</main>
