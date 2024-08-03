e84fb18c45b70dcba8d350b993b87479fa549237
rrivera <rrivera@redbannermedia.com>
2024-08-03 01:18:51 -0400

    EOD working on updating users

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,26 +8,30 @@ import LockIcon from '../components/icons/LockIcon.vue';
 import MailIcon from '../components/icons/MailIcon.vue';
 import StepCard from '../components/cards/StepCard.vue';
 
-import { reactive, computed } from 'vue';
-import { required, email, minLength, sameAs, alpha, alphaNum } from '@vuelidate/validators';
+import { reactive, computed, ref } from 'vue';
+import { email, minLength, alpha, alphaNum } from '@vuelidate/validators';
 import { useVuelidate } from '@vuelidate/core';
+import { useUserStore } from '../stores/user';
+import { postApi } from '../axios.ts';
+
+const userStore = useUserStore();
+const errorMessage = ref<string>('');
+const failedLogin = ref<boolean>(false);
 
 const form = reactive({
 	first_name: '',
 	last_name: '',
 	email: '',
 	username: '',
-	password: '',
-	confirmPassword: '',
 });
 
 // Rules for vuelidate validation
 const rules = computed(() => {
 	return {
-		first_name: { required, alpha },
-		last_name: { required, alpha },
-		email: { required, email },
-		username: { required, minLength: minLength(6), alphaNum },
+		first_name: { alpha },
+		last_name: { alpha },
+		email: { email },
+		username: { minLength: minLength(6), alphaNum },
 	}
 });
 
@@ -38,19 +42,17 @@ const submitForm = async () => {
 	const result = await v$.value.$validate();
 	if (!result) return
 
+	//TODO: remove any empty fields from the form
 	try {
-		const response = await postApi('/user', form);
+		const response = await postApi('/update-user', form);
 		if (response.status) {
 			failedLogin.value = true;
 			return
 		}
 
-		if (response.token) {
-			localStorage.setItem('token', response.token);
-			useUserStore().setUser(response.user);
-			router.push({ name: 'dashboard' });
-		}
 	} catch (error) {
+		errorMessage.value = error.response?.message.split('.')[0];
+		failedLogin.value = true;
 		v$.value.$touch();
 		console.error(error);
 	}
@@ -59,9 +61,7 @@ const submitForm = async () => {
 /**
 * TODO: Add a method to submit the form
 * Add a method to delete the account
-* Add a method to change the password
 * Add a method to update User Information
-* Add a method to update User Avatar
 */
 </script>
 <template>
@@ -70,26 +70,27 @@ const submitForm = async () => {
 		<main class="flex flex-col place-items-center w-5/12 mx-auto flex-grow space-y-24">
 			<ProfilePic />
 			<form class="flex flex-col place-items-center" @submit.prevent="submitForm">
-				<StepCard class="w-[49rem] h-auto drop-shadow-2xl border-black border-2 flex flex-col content-between space-y-12">
-					<h5 class="font-antonio text-3xl text-left">User Settings</h5>
+				<StepCard
+					class="w-[49rem] h-auto drop-shadow-2xl border-black border-2 flex flex-col content-between space-y-12">
+					<h5 class="font-antonio text-3xl text-left">Update User Profile</h5>
 					<div class="flex flex-row justify-between gap-x-2 content-between">
 						<TextInput type="text" label="First Name" placeholder="First Name" class="w-full relative"
 							v-model="form.first_name" :errors="v$.first_name.$errors">
-							<UserIcon svgClass="absolute -top-8 right-0 w-8 h-8 fill-[#3F3D56]" />
+							<UserIcon />
 						</TextInput>
 						<TextInput type="text" label="Last Name" placeholder="Last Name" class="w-full relative"
 							v-model="form.last_name" :errors="v$.last_name.$errors">
-							<UserIcon svgClass="absolute -top-8 right-0 w-8 h-8 fill-[#3F3D56]" />
+							<UserIcon />
 						</TextInput>
 					</div>
 					<div class="flex flex-row justify-between gap-x-2 !mt-1.5">
 						<TextInput type="text" label="Email" placeholder="Email" class="w-full relative" v-model="form.email"
 							:errors="v$.email.$errors">
-							<MailIcon class="absolute top-2 right-1.5" />
+							<MailIcon class="absolute top-1 right-1.5" />
 						</TextInput>
 						<TextInput type="text" label="Username" placeholder="Username" class="w-full relative"
 							v-model="form.username" :errors="v$.username.$errors">
-							<UserIcon svgClass="absolute -top-8 right-0 w-8 h-8 fill-[#3F3D56]" />
+							<UserIcon />
 						</TextInput>
 					</div>
 					<div class="flex flex-row justify-end space-x-3">
