fa9dedc35d54d3794584326bad612ce5a0a28448
rrivera <rrivera@redbannermedia.com>
2024-08-15 17:08:50 -0400

    Added layout component

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import NotFoundIcon from '../components/icons/NotFoundIcon.vue';
+import Layout from '../components/Layouts/Layout.vue';
 </script>
 <template>
-	<div class="flex flex-col h-screen justify-center place-items-center space-y-4">
-		<h1 class="text-5xl text-center font-antonio text-shadow">
-			404 Page Not Found
-		</h1>
-		<NotFoundIcon class="size-96" />
-	</div>
+	<Layout :footer="false">
+		<template #content>
+			<div class="flex flex-col h-full justify-center place-items-center space-y-4 overflow-y-hidden">
+				<h1 class="text-5xl text-center font-antonio text-shadow">
+					404 Page Not Found
+				</h1>
+				<NotFoundIcon class="size-96" />
+			</div>
+		</template>
+	</Layout>
 </template>
