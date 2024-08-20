29700229bd2a8d5363450300d30c3a2ac85a52ad
Roberto Rivera <rrivera@redbannermedia.com>
2024-08-19 22:10:32 -0400

    Cleaned up TextInput

--- src/components/inputs/TextInput.vue
+++ src/components/inputs/TextInput.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import { ErrorObject } from "@vuelidate/core";
+
 defineProps<{
     type: string;
     label: string;
     placeholder: string;
     modelValue: string;
-    errors: object | null;
+    errors: ErrorObject[];
 }>();
-</script>
+</script
 <template>
     <div class="flex flex-col h-16">
         <label class="relative" :for="label">
@@ -15,17 +17,17 @@ defineProps<{
                 :id="label"
                 :placeholder="placeholder"
                 :value="modelValue"
-                @input="$emit('update:modelValue', $event?.target?.value)"
+                @input="$emit('update:modelValue', modelValue)"
                 class="w-full p-2 placeholder:text-custom-coal/75 text-custom-coal rounded-lg bg-zinc-300 shadow-[inset_1px_4px_8px_0_rgba(47,46,65,0.6)]"
             />
             <slot class="absolute top-0 right-0" />
         </label>
         <p
-            v-for="error in errors"
-            :key="error"
+            v-for="(error, index) in errors"
+            :key="index"
             class="text-red-500 text-left text-shadow"
         >
-            {{ error?.$message }}
+            {{ error.$message }}
         </p>
     </div>
 </template>
